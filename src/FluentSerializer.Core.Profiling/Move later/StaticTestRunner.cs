198dbd5a9fed7599d738aac1e0abc773f8ec9c54
Marvin Brouwer <marvin-brouwer@live.nl>
2021-10-16 20:34:40 +0200

    OTHER Simplified profiler

---
+++ src/FluentSerializer.Core.Profiling/Move later/StaticTestRunner.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+
+namespace FluentSerializer.Core.Profiling.Move
+{
+    public abstract class StaticTestRunner
+    {
+        protected static ManualConfig CreateConfig()
+        {
+            var config = ManualConfig
+                .CreateMinimumViable()
+                .AddExporter(PlainExporter.Default)
+                .AddJob(Job.Dry
+                    .WithLaunchCount(5)
+                    .WithMaxRelativeError(0.01)
+                    .WithId(typeof(BenchmarkRunner).Assembly.FullName)
+                );
+            #if (DEBUG)
+            config = config.WithOptions(ConfigOptions.DisableOptimizationsValidator);
+            #endif
+
+            return config;
+        }
+
+        public static void Run(string[] parameters, System.Collections.Generic.IEnumerable<Type> profileDefinitions)
+        {
+            var config = CreateConfig();
+            var tests = profileDefinitions.ToArray();
+
+            if (!parameters.Any())
+                BenchmarkSwitcher.FromTypes(tests).RunAllJoined(config);
+            BenchmarkSwitcher.FromTypes(tests).Run(parameters, config);
+        }
+    }
+}
