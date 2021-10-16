9fc02c6b28e0b5109f6f9367507b39cbb5531869
Marvin Brouwer <marvin-brouwer@live.nl>
2021-10-16 20:34:40 +0200

    OTHER Simplified profiler

--- src/FluentSerializer.Core.Profiling/BenchmarkTests.cs
+++ src/FluentSerializer.Core.Profiling/BenchmarkTests.cs
@@ -2,28 +2,16 @@ using FluentSerializer.Core.Profiling.Data;
 using FluentSerializer.Core.Profiling.Move;
 using System;
 using System.Collections.Generic;
-using Xunit;
-using Xunit.Abstractions;
 
 namespace FluentSerializer.Core.Profiling
 {
-    public sealed class BenchmarkTests : TestDataConfiguration
+    public static class BenchmarkTests
     {
-        public override IEnumerable<Type> GetTestClasses()
+        [STAThread] public static void Main(params string[] parameters) => StaticTestRunner.Run(parameters, GetTestClasses());
+
+        public static IEnumerable<Type> GetTestClasses()
         {
             yield return typeof(DataCompilerProfile);
         }
-    }
-
-    public static class Program
-    {
-        [STAThread] public static void Main(params string[] parameters) => JoinedTestRunner.Run<BenchmarkTests>(parameters);
-    }
-    public sealed class TestRunner : JoinedTestRunner
-    {
-        public TestRunner(ITestOutputHelper output) : base(output) { }
-
-        [Theory, ClassData(typeof(BenchmarkTests))]
-        public override void ProfileInTestExplorer(Type type) => base.ProfileInTestExplorer(type);
     }
 }
