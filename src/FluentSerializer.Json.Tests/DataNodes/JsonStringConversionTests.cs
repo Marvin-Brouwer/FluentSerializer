634270ce9caadd731e418254325effb772ddace7
marvin.brouwer <marvin.brouwer@valtech.com>
2021-10-20 16:59:06 +0200

    OTHER Rename

--- src/FluentSerializer.Json.Tests/DataNodes/JsonStringConversionTests.cs
+++ src/FluentSerializer.Json.Tests/DataNodes/JsonStringConversionTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using FluentSerializer.Json.DataNodes;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace FluentSerializer.Json.Tests.DataNodes
     {
         private readonly JsonObject _testObject;
         private readonly string _testJsonFormatted;
-        private readonly string _TestJsonSlim;
+        private readonly string _testJsonSlim;
 
         public JsonStringConversionTests
 ()
@@ -29,21 +30,35 @@ namespace FluentSerializer.Json.Tests.DataNodes
 
             _testJsonFormatted = "{\r\n\t\"prop\" : \"Test\",\r\n\t\"prop2\" : {\r\n\t\t\"array\" : " +
                 "[\r\n\t\t\t{\r\n\t\t\t},\r\n\t\t\t[\r\n\t\t\t]\r\n\t\t],\r\n\t\t\"prop3\" : 1,\r\n\t\t\"prop4\" : true,\r\n\t\t\"prop5\" : null\r\n\t}\r\n}";
-            _TestJsonSlim = "{\"prop\":\"Test\",\"prop2\":{\"array\":[{},[]],\"prop3\":1,\"prop4\":true,\"prop5\":null}}";
+            _testJsonSlim = "{\"prop\":\"Test\",\"prop2\":{\"array\":[{},[]],\"prop3\":1,\"prop4\":true,\"prop5\":null}}";
 
         }
-
+        
         [Theory, InlineData(true), InlineData(false)]
         public void JsonObjectToString(bool format)
         {
             // Arrange
-            var expected = format ? _testJsonFormatted : _TestJsonSlim;
+            var expected = format ? _testJsonFormatted : _testJsonSlim;
 
             // Act
             var result = _testObject.ToString(format);
 
             // Assert
             result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory, InlineData(true), InlineData(false)]
+        public void StringToObject(bool format)
+        {
+            // Arrange
+            var expected = _testObject;
+            var input = format ? _testJsonFormatted : _testJsonSlim;
+
+            // Act
+            var result = JsonObject.FromSpan(input.AsSpan());
+
+            // Assert
+            result.instance.Should().BeEquivalentTo(expected);
         }
     }
 }
