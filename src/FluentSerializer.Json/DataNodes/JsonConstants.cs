0c3753d89117600d85da97601b8968f3e6acedc1
Marvin Brouwer <marvin-brouwer@live.nl>
2021-10-24 00:57:50 +0200

    OTHER Cleaned up Json types and moved them to records

---
+++ src/FluentSerializer.Json/DataNodes/JsonConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentSerializer.Json.DataNodes
+{
+    internal readonly struct JsonConstants
+    {
+
+        internal const char ObjectStartCharacter = '{';
+        internal const char ObjectEndCharacter = '}';
+
+        internal const char ArrayStartCharacter = '[';
+        internal const char ArrayEndCharacter = ']';
+
+        internal const char PropertyWrapCharacter = '"';
+        internal const char PropertyAssignmentCharacter = ':';
+
+        internal const char DividerCharacter = ',';
+
+        internal const string NullValue = "null";
+    }
+}
