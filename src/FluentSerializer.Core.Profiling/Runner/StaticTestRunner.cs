5fbd72852a7a029c5742b897788b7b718b42887d
Marvin Brouwer <marvin-brouwer@live.nl>
2021-12-07 21:56:51 +0100

    Issue#5 trying with C#

--- src/FluentSerializer.Core.Profiling/Runner/StaticTestRunner.cs
+++ src/FluentSerializer.Core.Profiling/Runner/StaticTestRunner.cs
@@ -11,6 +11,10 @@ using System.Diagnostics;
 using System.Security.Principal;
 using System.Runtime.InteropServices;
 using Perfolizer.Horology;
+using System.Collections.Generic;
+using BenchmarkDotNet.Columns;
+using System.IO;
+using System.Linq;
 
 #if (DEBUG)
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
@@ -22,16 +26,14 @@ namespace FluentSerializer.Core.Profiling.Runner
     {
         private static ManualConfig CreateConfig()
         {
-            var config = ManualConfig.Create(DefaultConfig.Instance)
-                .AddExporter(PlainExporter.Default)
+			var config = ManualConfig.Create(DefaultConfig.Instance)
                 .WithOrderer(new GroupedSlowestToFastestOrderer())
                 .AddJob(CreateJob(CoreRuntime.Core31))
                 .AddJob(CreateJob(CoreRuntime.Core50))
-                .AddExporter(PlainExporter.Default, MarkdownExporter.GitHub);
+                .AddExporter(MarkdownExporter.GitHub);
 #if (DEBUG)
             config = config.WithOptions(ConfigOptions.DisableOptimizationsValidator);
 #endif
-
             return config;
         }
 
@@ -54,7 +56,7 @@ namespace FluentSerializer.Core.Profiling.Runner
         }
 
 		[PrincipalPermission(SecurityAction.Demand, Role = @"BUILTIN\Administrators")]
-		public static void Run(string[] parameters, Assembly assembly)
+		public static void Run(Assembly assembly, string dataType)
 		{
 			RequireElevatedPermissions();
 
@@ -66,6 +68,34 @@ namespace FluentSerializer.Core.Profiling.Runner
 			Console.WriteLine();
 
 			BenchmarkSwitcher.FromAssembly(assembly).RunAllJoined(config);
+
+			FixFileNames(dataType, config);
+		}
+
+		/// <summary>
+		/// Manually fix filenames, the markdown exporter doesn't allow for inheritance so we'll fix it ourselves;
+		/// </summary>
+		/// <param name="dataType"></param>
+		private static void FixFileNames(string dataType, ManualConfig config)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine();
+			Console.WriteLine("Correcting fileName");
+			Console.ResetColor();
+
+			var resultsDir = new DirectoryInfo(Path.Join(config.ArtifactsPath, "results"));
+			var markdownSummaryFile = resultsDir
+				.GetFiles("*.md")
+				.OrderByDescending(directory => directory.CreationTimeUtc)
+				.FirstOrDefault();
+
+			var readableFileName = markdownSummaryFile.FullName
+				.Replace("BenchmarkRun-joined", $"{dataType}-benchmark")
+				.Replace("-report-github", string.Empty);
+
+			Console.WriteLine($"Renaming report to \"{readableFileName}\"");
+			Console.WriteLine();
+			markdownSummaryFile.MoveTo(readableFileName);
 		}
 
 		public static void RequireElevatedPermissions()
@@ -81,9 +111,11 @@ namespace FluentSerializer.Core.Profiling.Runner
 
 			// Restart program and run as admin
 			var exeName = Process.GetCurrentProcess().MainModule.FileName;
-			var startInfo = new ProcessStartInfo(exeName);
-			startInfo.UseShellExecute = true;
-			startInfo.Verb = "runas";
+			var startInfo = new ProcessStartInfo(exeName)
+			{
+				UseShellExecute = true,
+				Verb = "runas"
+			};
 
 			Process.Start(startInfo);
 			Environment.Exit(0);
