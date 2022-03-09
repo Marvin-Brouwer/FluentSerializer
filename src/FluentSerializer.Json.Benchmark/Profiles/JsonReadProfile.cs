73bf156ad23df61b542ff1293ca4468722c8479d
Marvin Brouwer <marvin-brouwer@live.nl>
2022-03-09 19:19:26 +0100

    #56 Removed duplicate benchmark

--- src/FluentSerializer.Json.Benchmark/Profiles/JsonReadProfile.cs
+++ src/FluentSerializer.Json.Benchmark/Profiles/JsonReadProfile.cs
@@ -17,7 +17,4 @@ public class JsonReadProfile : ReadProfile
 
 	[Benchmark, BenchmarkCategory(nameof(ReadJson))]
 	public IJsonObject ReadJson() => JsonParser.Parse(CaseReader.ReadToEnd());
-
-	[Benchmark, BenchmarkCategory(nameof(ReadJson))]
-	public IJsonObject ReadJson2() => JsonParser.Parse(CaseReader.ReadToEnd());
 }
