7f4dd6c2274d2f0ad5cc641c1ebf7abf3577775d
marvin.brouwer <marvin.brouwer@valtech.com>
2022-03-17 09:45:58 +0100

    #104 fixing version fix issues

--- .github/workflows/update-version.ps1
+++ .github/workflows/update-version.ps1
@@ -7,6 +7,10 @@ Write-Host "Updating version in $file to $version" -ForegroundColor DarkYellow;
 
 [xml]$xmlDoc = Get-Content $File;
 
-$xmlDoc.Project.PropertyGroup[0].Version = $Version;
+try {
+	$xmlDoc.Project.PropertyGroup[0].Version = $Version;
+} catch {
+	$xmlDoc.Project.PropertyGroup.Version = $Version;
+}
 
 $xmlDoc.Save($File);
