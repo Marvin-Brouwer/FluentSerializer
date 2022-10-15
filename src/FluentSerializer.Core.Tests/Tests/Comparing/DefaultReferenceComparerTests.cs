a89a665db10b81e1e5bdb2b909a8b7b63858efea
Marvin Brouwer <marvin-brouwer@live.nl>
2022-10-15 12:50:28 +0200

    [#93] Created DefaultReferenceComparerTests

---
+++ src/FluentSerializer.Core.Tests/Tests/Comparing/DefaultReferenceComparerTests.cs
@@ -0,0 +1,89 @@
+using FluentAssertions;
+
+using FluentSerializer.Core.Comparing;
+
+using Xunit;
+
+
+namespace FluentSerializer.Core.Tests.Tests.Comparing;
+
+public sealed class DefaultReferenceComparerTests
+{
+	private static readonly DefaultReferenceComparer Sut = DefaultReferenceComparer.Default;
+
+	[Fact]
+	public void Equals_BothAreNull_ReturnsTrue()
+	{
+		// Arrange
+		int? a = null;
+		string? b = null;
+
+		// Act
+		var result = Sut.Equals(a, b);
+
+		// Assert
+		result.Should().BeTrue();
+	}
+
+	[Fact]
+	public void Equals_OneIsNull_ReturnsFalse()
+	{
+		// Arrange
+		string? a = null;
+		var b = "b";
+		string? c = null;
+
+		// Act
+		var resultLeft = Sut.Equals(a, b);
+		var resultRight = Sut.Equals(c, b);
+
+		// Assert
+		resultLeft.Should().BeFalse();
+		resultRight.Should().BeFalse();
+	}
+
+	[Fact]
+	public void Equals_ReferenceEqual_ReturnsTrue()
+	{
+		// Arrange
+		var a = "Same";
+		var b = a;
+
+		// Act
+		var result = Sut.Equals(a, b);
+
+		// Assert
+		result.Should().BeTrue();
+	}
+
+	[Fact]
+	public void Equals_AreEqual_ReturnsTrue()
+	{
+		// Arrange
+		var a = "Same";
+		var b = "Same";
+
+		// Act
+		var result = Sut.Equals(a, b);
+
+		// Assert
+		result.Should().BeTrue();
+	}
+
+	[Fact]
+	public void Equals_AreNotEqual_ReturnsFalse()
+	{
+		// Arrange
+		var a = "A";
+		var b = "B";
+		var c = "C";
+
+		// Act
+		var resultName = Sut.Equals(a, b);
+		var resultValue = Sut.Equals(b, c);
+
+		// Assert
+		resultName.Should().BeFalse();
+		resultValue.Should().BeFalse();
+	}
+}
