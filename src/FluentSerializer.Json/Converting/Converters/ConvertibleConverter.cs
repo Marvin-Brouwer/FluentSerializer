1fb3ac1aa4419c0cd4edfb725f6730c4d79ae889
Marvin Brouwer <marvin-brouwer@live.nl>
2022-11-08 10:29:20 +0100

    [#93] Made stringconversion for JSON more strict

--- src/FluentSerializer.Json/Converting/Converters/ConvertibleConverter.cs
+++ src/FluentSerializer.Json/Converting/Converters/ConvertibleConverter.cs
@@ -4,6 +4,7 @@ using FluentSerializer.Core.DataNodes;
 using FluentSerializer.Json.DataNodes;
 
 using System;
+using System.Data;
 using System.Globalization;
 
 using static FluentSerializer.Json.JsonBuilder;
@@ -38,9 +39,21 @@ public sealed class ConvertibleConverter : ConvertibleConverterBase, IJsonConver
 	public object? Deserialize(in IJsonNode objectToDeserialize, in ISerializerContext<IJsonNode> context)
 	{
 		if (objectToDeserialize is not IDataValue data) return default;
-		if (context.PropertyType == typeof(string) && data.Value?.Length > 2)
-			return data.Value[1..^1];
+
+		if (context.PropertyType == typeof(string)) return HandleStringData(in data);
 
 		return ConvertToNullableDataType(data.Value, context.PropertyType);
+	}
+
+	/// <remarks>
+	/// The <see cref="ConvertibleConverterBase.ConvertToNullableDataType"/> can probably handle string very well.
+	/// However, because of how the quotes work in JSON, we'd like some more control when it comes to strings.
+	/// </remarks>
+	private static string? HandleStringData(in IDataValue data)
+	{
+		if (data.Value is null || data.Value.Length == 0) return default;
+		if (data.Value?.Length >= 2) return data.Value[1..^1];
+
+		throw new DataException("A string type cannot be smaller than 2 since it should be surrounded in quotes");
 	}
 }
