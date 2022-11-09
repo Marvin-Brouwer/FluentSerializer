7b0d07e6d8d5673a84d18428a70256fec94d4128
Marvin Brouwer <marvin-brouwer@live.nl>
2022-11-09 10:30:26 +0100

    [#93] Added initializer tests for JSON Nodes

--- src/FluentSerializer.Json.Tests/Tests/DataNodes/Nodes/JsonCommentMultilineTests.cs
+++ src/FluentSerializer.Json.Tests/Tests/DataNodes/Nodes/JsonCommentMultilineTests.cs
@@ -1,5 +1,13 @@
+using FluentAssertions;
+
+using FluentSerializer.Core.TestUtils.Extensions;
 using FluentSerializer.Core.TestUtils.Helpers;
 using FluentSerializer.Core.Text;
+using FluentSerializer.Json.DataNodes.Nodes;
+
+using Xunit;
+
+using static FluentSerializer.Json.JsonBuilder;
 
 namespace FluentSerializer.Json.Tests.Tests.DataNodes.Nodes;
 
@@ -10,5 +18,50 @@ public sealed partial class JsonCommentMultiLineTests
 	public JsonCommentMultiLineTests()
 	{
 		_textWriter = TestStringBuilderPool.CreateSingleInstance();
+	}
+
+	[Fact,
+		Trait("Category", "UnitTest"), Trait("DataFormat", "JSON")]
+	public void InitializeComment_ValueNull_ReturnsEmpty()
+	{
+		// Arrange
+		var expected = (string?)null;
+		var input = (string)null!;
+
+		// Act
+		var result = new JsonCommentMultiLine(in input);
+
+		// Assert
+		result.Value.Should().BeEquivalentTo(expected);
+	}
+
+	[Fact,
+		Trait("Category", "UnitTest"), Trait("DataFormat", "JSON")]
+	public void InitializeComment_ValueEmpty_ReturnsEmpty()
+	{
+		// Arrange
+		var expected = string.Empty;
+		var input = string.Empty;
+
+		// Act
+		var result = new JsonCommentMultiLine(in input);
+
+		// Assert
+		result.Value.Should().BeEquivalentTo(expected);
+	}
+
+	[Fact,
+		Trait("Category", "UnitTest"), Trait("DataFormat", "JSON")]
+	public void InitializeComment_HasValue_ReturnsWithChild()
+	{
+		// Arrange
+		var expected = MultilineComment("I have a value!");
+		var input = "I have a value!";
+
+		// Act
+		var result = new JsonCommentMultiLine(in input);
+
+		// Assert
+		result.Should().BeEquatableTo(expected);
 	}
 }
