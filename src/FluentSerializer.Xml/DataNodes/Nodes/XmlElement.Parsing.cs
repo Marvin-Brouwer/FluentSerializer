765ea1fd5839dc281878d93e11202b74ef874712
Marvin Brouwer <marvin-brouwer@live.nl>
2022-12-17 12:39:07 +0100

    [#183] Splitting XmlNodes parsing to reduce cognitive complexity

--- src/FluentSerializer.Xml/DataNodes/Nodes/XmlElement.Parsing.cs
+++ src/FluentSerializer.Xml/DataNodes/Nodes/XmlElement.Parsing.cs
@@ -2,6 +2,7 @@ using FluentSerializer.Core.Extensions;
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace FluentSerializer.Xml.DataNodes.Nodes;
 
@@ -12,6 +13,46 @@ public readonly partial struct XmlElement
 	/// <b>Please use <see cref="XmlParser.Parse"/> method instead of this constructor</b>
 	/// </remarks>
 	public XmlElement(in ReadOnlySpan<char> text, ref int offset)
+	{
+		_attributes = Array.Empty<IXmlAttribute>();
+		_children = Array.Empty<IXmlNode>();
+
+		MoveToElementStart(in text, ref offset);
+		offset.AdjustForWhiteSpace(in text);
+
+		Name = string.Empty;
+		Name = ParseElementName(in text, ref offset, out var elementClosed, out var tagFinished);
+
+		var attributes = new List<IXmlAttribute>();
+		ParseElementAttributes(in text, ref offset, ref attributes, ref elementClosed, ref tagFinished);
+
+		if (elementClosed)
+		{
+			_attributes = attributes.AsReadOnly();
+			return;
+		}
+
+		var children = new List<IXmlNode>();
+		ParseElementChildren(in text, ref offset, ref children);
+
+		// Walk to the end of the current closing tag
+		while (text.WithinCapacity(in offset))
+		{
+			if (text.HasCharacterAtOffset(in offset, XmlCharacterConstants.TagEndCharacter)) break;
+			offset++;
+		}
+		offset.AdjustForToken(XmlCharacterConstants.TagEndCharacter);
+
+		_attributes = attributes.AsReadOnly();
+		_children = children.AsReadOnly();
+	}
+
+#if NET6_0_OR_GREATER
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+	private static void MoveToElementStart(in ReadOnlySpan<char> text, ref int offset)
 	{
 		// If we encounter a declaration just ignore it, if this becomes a problem we can start the parse in
 		// the document. For now this is fine.
@@ -28,16 +69,21 @@ public readonly partial struct XmlElement
 			offset++;
 		}
 		offset.AdjustForToken(XmlCharacterConstants.TagStartCharacter);
-		offset.AdjustForWhiteSpace(in text);
+	}
 
-		var attributes = new List<IXmlAttribute>();
-		var children = new List<IXmlNode>();
+#if NET6_0_OR_GREATER
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+	private static string ParseElementName(
+		in ReadOnlySpan<char> text, ref int offset, out bool elementClosed, out bool tagFinished)
+	{
+		tagFinished = false;
+		elementClosed = false;
 
 		var nameStartOffset = offset;
 		var nameEndOffset = offset;
-
-		var elementClosed = false;
-		var tagFinished = false;
 		var nameFinished = false;
 
 		while (text.WithinCapacity(in offset))
@@ -45,8 +91,8 @@ public readonly partial struct XmlElement
 			nameEndOffset = offset;
 
 			if (text.HasCharactersAtOffset(in offset,
-			    XmlCharacterConstants.TagTerminationCharacter,
-			    XmlCharacterConstants.TagEndCharacter))
+				XmlCharacterConstants.TagTerminationCharacter,
+				XmlCharacterConstants.TagEndCharacter))
 			{
 				elementClosed = true;
 				offset.AdjustForToken(XmlCharacterConstants.TagTerminationCharacter);
@@ -63,20 +109,30 @@ public readonly partial struct XmlElement
 
 			if (nameFinished) break;
 
-			offset++;
+			offset.Increment();
 			if (text.HasWhitespaceAtOffset(in offset))
 			{
 				nameFinished = true;
 			}
 		}
-            
-		Name = text[nameStartOffset..nameEndOffset].ToString().Trim();
 
+		return text[nameStartOffset..nameEndOffset].ToString().Trim();
+	}
+
+#if NET6_0_OR_GREATER
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+	private static void ParseElementAttributes(
+		in ReadOnlySpan<char> text, ref int offset, ref List<IXmlAttribute> attributes,
+		ref bool elementClosed, ref bool tagFinished)
+	{
 		while (!tagFinished && text.WithinCapacity(in offset))
 		{
 			if (text.HasCharactersAtOffset(in offset,
-				    XmlCharacterConstants.TagTerminationCharacter,
-				    XmlCharacterConstants.TagEndCharacter))
+					XmlCharacterConstants.TagTerminationCharacter,
+					XmlCharacterConstants.TagEndCharacter))
 			{
 				offset.AdjustForToken(XmlCharacterConstants.TagTerminationCharacter);
 				elementClosed = true;
@@ -97,25 +153,26 @@ public readonly partial struct XmlElement
 
 			attributes.Add(new XmlAttribute(in text, ref offset));
 		}
+	}
 
-		if (elementClosed)
-		{
-			_attributes = attributes.AsReadOnly();
-			_children = children.AsReadOnly();
-			return;
-		}
-
+#if NET6_0_OR_GREATER
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+	private static void ParseElementChildren(in ReadOnlySpan<char> text, ref int offset, ref List<IXmlNode> children)
+	{
 		while (text.WithinCapacity(in offset))
 		{
 			if (text.HasCharactersAtOffset(in offset,
-				    XmlCharacterConstants.TagStartCharacter,
-				    XmlCharacterConstants.TagTerminationCharacter))
+					XmlCharacterConstants.TagStartCharacter,
+					XmlCharacterConstants.TagTerminationCharacter))
 			{
 				offset.AdjustForToken(XmlCharacterConstants.TagStartCharacter);
 				offset.AdjustForToken(XmlCharacterConstants.TagTerminationCharacter);
 				break;
 			}
-			
+
 			if (text.HasCharacterAtOffset(in offset, XmlCharacterConstants.TagStartCharacter))
 			{
 				if (text.HasCharactersAtOffset(in offset, XmlCharacterConstants.CommentStart))
@@ -140,16 +197,5 @@ public readonly partial struct XmlElement
 
 			children.Add(new XmlText(in text, ref offset));
 		}
-
-		// Walk to the end of the current closing tag
-		while (text.WithinCapacity(in offset))
-		{
-			if (text.HasCharacterAtOffset(in offset, XmlCharacterConstants.TagEndCharacter)) break;
-			offset++;
-		}
-		offset.AdjustForToken(XmlCharacterConstants.TagEndCharacter);
-
-		_attributes = attributes.AsReadOnly();
-		_children = children.AsReadOnly();
 	}
 }
