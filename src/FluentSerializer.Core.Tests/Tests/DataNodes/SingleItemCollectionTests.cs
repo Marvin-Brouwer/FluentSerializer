f625ad7d0bc7af175ac5c4a2a1a962132e81f742
Marvin Brouwer <marvin-brouwer@live.nl>
2023-04-15 16:06:24 +0200

    [OTHER] Fixed build warnings

--- src/FluentSerializer.Core.Tests/Tests/DataNodes/SingleItemCollectionTests.cs
+++ src/FluentSerializer.Core.Tests/Tests/DataNodes/SingleItemCollectionTests.cs
@@ -2,5 +2,7 @@ namespace FluentSerializer.Core.Tests.Tests.DataNodes;
 
 public sealed partial class SingleItemCollectionTests
 {
+#pragma warning disable S2094 // Classes should not be empty
 	private sealed class TestClass { }
+#pragma warning restore S2094 // Classes should not be empty
 }
